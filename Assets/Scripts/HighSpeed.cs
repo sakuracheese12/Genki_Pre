b855d2b42ed24797ed815b3c7135d5e923b50d4b
Your Name <you@example.com>
2016-08-31 21:25:03 +0900

    make highspeed

---
+++ Assets/Scripts/HighSpeed.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighSpeed : MonoBehaviour {
+
+    public GameObject player;
+    public float power = 2;
+
+    private Rigidbody prb;
+    private Rigidbody hrb;
+
+    void Start()
+    {
+        prb = player.GetComponent<Rigidbody>();
+        hrb = GetComponent<Rigidbody>();
+
+        hrb.maxAngularVelocity = prb.maxAngularVelocity * power;
+    }
+
+    void Update()
+    {
+        hrb.angularVelocity = prb.angularVelocity * power;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position;
+
+    }
+
+}
